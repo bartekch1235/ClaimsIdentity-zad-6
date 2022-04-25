fe19138d5b3db3600b1e02a00dc2fa3c6e70ea31
Bartłomiej Chmielewski <gumif@wp.pl>
2022-04-25 12:00:13 +0200

    Add project files.

---
+++ ClaimsIdentity zad 6/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClaimsIdentity_zad_6.Pages
+{
+    public class IndexModel : PageModel
+    {
+  
+        private readonly Microsoft.AspNetCore.Identity.UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public IndexModel(
+            Microsoft.AspNetCore.Identity.UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+            if (User.Identity.GetUserId() != null)
+            {
+                var user =  _userManager.GetUserName(User);
+                ViewData["email"] = user.ToString();
+            }
+        }
+    }
+}
